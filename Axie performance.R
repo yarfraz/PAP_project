7ed961e8e8f597403064efd0e2e1242453ddec4c
unknown <yarfraznazuddeen@gmail.com>
2022-01-15 12:00:20 -0400

    Add script & data

---
+++ Axie performance.R
@@ -0,0 +1,20 @@
+### This is a short project about my performance as an Axie Infinity scholar
+### The data recolects my daily wins, loses, MMR, and SLP production over 3 months
+### (from September to December)
+### It also contains some extra cols indicating time, day, and month. 
+### My objetives are
+
+### - Clean the data to only have records from September to December
+### - Obtain the following measurments of my performance: 
+###   - Totals: 
+###     - SLP production
+###     - Won and lost matches
+###     - Winrate
+###   - Average: 
+###     - SLP production by month 
+### - Create the following graphs and tables 
+###   - Monthly SLP production 
+###   - Monthly avg SLP production 
+###   - Max & min MMR per month  
+###   - Timetable of daily slp production
+###   - Timetable of daily MMR results 
